ca0032e0dc98c6171f220562baf56fb2124e38be
swdsm100040 <swdsm100040@gmail.com>
2020-10-01 19:12:46 +0530

    completed

---
+++ thank-you.php
@@ -0,0 +1,25 @@
+<?php require_once("include_about/header.php"); ?>
+
+
+
+	  <div class="error_page">
+        <div class="container">
+            <div class="row">
+			 <div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12 text-center">
+                    <div class="error_page_cntnt">
+                        <h2>
+                            <span>THANK</span><br><br>
+                            <span>YOU</span>
+                        </h2>
+                        <h3>Your Submission is received we will contact you soon.(</h3>
+                        <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. A Aenean sollicitudin, lorem quis bibend Aenean sollicitudin, lorem . <a href="index.php">Home Page</a></p><br><br>
+                         <p class="lead">
+                                    <a class="btn btn-primary btn-sm" href="index.Php" role="button" style="color: #fff;background-color: #346eb6;border-color: #346eb6; padding: 11px 14px;font-size: 18px; border-radius: 15px;">Continue to homepage</a>
+                                </p>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+	
+ <?php require_once("include_about/footer.php"); ?>
